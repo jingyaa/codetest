6d57be44bd73a7d6497c5ef46f6735d072b5fdb1
jingya <ya.jing@cripac.ia.ac.cn>
2020-08-12 21:30:34 +0800

    title.py

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-print('hello')
+def sort_arr(arr):
+    res = arr.sort()
+
+    return res
+
+import sys
+
+if __name__ == "__main__":
+    mn = sys.stdin.readline().strip()
+    mn = map(int, mn.split())
+    n = mn[0]
+    m = mn[1]
+    lines1 = sys.stdin.readline().strip()
+    arr = map(int, lines1.split())
+    for i in range(m):
+        value_i = sys.stdin.readline().strip()
+        value_i = map(int, value_i.split())
+        print('B')
+
 
 
