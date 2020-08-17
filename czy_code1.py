c575312ff610d9807c187149eedca8208a0903b9
jingyaa <38721860+jingyaa@users.noreply.github.com>
2020-08-17 21:37:07 +0800

    Delete czy_code1.py

--- czy_code1.py
+++
@@ -1,8 +0,0 @@
-# hello my name is cuizeyu, i will rewrite this file~
-
-with open("czy_input1.txt") as f:
-    num = list(map(int, f.readline().strip().split(' ')))
-num.sort()
-
-print "hahahahhahahhahha "
-print num
